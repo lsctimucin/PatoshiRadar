79e515eecd696d02c8737e75dab7b5eb242b7f77
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-07-31 17:43:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from pump_monitor import PumpMonitor
+from telegram_sender import send_message
 
 
 def new_token(data):
 
-    print("===================================")
-    print(data)
-    print("===================================")
+    name = data.get("name", "Bilinmiyor")
+    symbol = data.get("symbol", "-")
+    mint = data.get("mint", "")
+    market_cap = data.get("marketCapSol", 0)
+
+    message = f"""
+🚀 Yeni Coin Bulundu!
+
+📛 İsim: {name}
+💎 Sembol: {symbol}
+💰 Market Cap: {market_cap} SOL
+
+https://pump.fun/{mint}
+"""
+
+    print(message)
+
+    send_message(message)
 
 
 monitor = PumpMonitor(new_token)
