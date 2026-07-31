8e35aac2c9a89066369c4868723a344e1f9218c7
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-07-31 16:35:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from config import BOT_TOKEN, CHAT_ID
+from telegram_sender import TelegramSender
+
 print("Patoshi Radar Başlatılıyor...")
+
+bot = TelegramSender(BOT_TOKEN, CHAT_ID)
+
+bot.send("✅ Patoshi Radar çalışmaya başladı.")
