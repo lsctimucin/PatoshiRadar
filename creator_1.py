83bcf3d629629dddce4b738dd7c4fc9ccddd7f38
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-08-03 13:33:42 +0300

    Update creator_1.py

--- creator_1.py
+++ creator_1.py
@@ -2,4 +2,41 @@
 
 TARGET_CREATORS = {
     "Patoshi Team": "GVyByLjvURtf1wyNkfyJbQGrnjNo8uhAH7J6u197Cpap",
+    "Patoshi Team": "J8x8BFQrQe6xSwXEhmJFXmkkiMQ54N71ZQ5bmL9sFs4b",
+    "Patoshi Team": "7XJhvpp9X16Ynx2tgbQVH7exL6PTZLkEA6FxdpFYwebX",
+    "Patoshi Team": "Fe74jVetvQfqZ6J1xMNPpZ5SGQJUFxTyydPEyAk16pGR",
+    "Patoshi Team": "DSxyPrKxaDxeEJZ4Nb8cx8qsRNexw1W6UHmvqgEAA4hq",
+    "Patoshi Team": "GVyBRxSMpZEyzb3Cj2QiF9fRxV9xtgCUtz9vQ7fSCpap",
+    "Patoshi Team": "9nUNwTjTZASbbK9cigF7WKbo16oQ97uDG5kb38j4nd8H",
+    "Patoshi Team": "C68a6RCGLiPskbPYtAcsCjhG8tfTWYcoB4JjCrXFdqyo",
+    "Patoshi Team": "5tzFkiKscXHK5ZXCGbXZxdw7gTjjD1mBwuoFbhUvuAi9",
+    "Patoshi Team": "B9D2m5ghdPC9WJXw98cie3mzrLTDM1NaHkJwWRdNnGVx",
+    "Patoshi Team": "GVyBBoE1oNFkLdP7c9NcZTHqg9uSSDpSwYM9iED8Cpap",
+    "Patoshi Team": "68xXNr9bkknyP8W4umBniKZqx8JB4p2xvU4pK2tUPnGu",
+    "Patoshi Team": "6SCbq9k192ZHXTdduVgCW1oj24G9qoj1PF1xqeffBNjU",
+    "Patoshi Team": "FbymH9BbHvUrmtcUrRW8Rz9sTv6QusJMbytuwvSN2Qir",
+    "Patoshi Team": "6SCbq9k192ZHXTdduVgCW1oj24G9qoj1PF1xqeffBNjU",
+    "Patoshi Team": "5K5RtTWzzLp4P8Npi84ocf7F1vBsAu29N1irG4iiUnzt",
+    "Patoshi Team": "4VtRaVgSjvM2g7sLy5bZUJ4xtQqjWn7PnRd6a5VQufEf",
+    "Patoshi Team": "8ndZXJs3hpgKQ3bfafTe1tEppPsUGQn2zqr7HMBLkp1F",
+    "Patoshi Team": "AVahywMVNRYzdgWrufSWrtdGXAeNEvfpJFxhVFK516mT",
+    "Patoshi Team": "5FJyYrQqaXePEgUpQ9t1CriJwXxJ4yAbHBJtcJUTwWRS",
+    "Patoshi Team": "CxBAA1mURqB6BT2mYAUHKcjK6iV5JjCoPdTzXCjH47uy",
+    "Patoshi Team": "6ZeLJuK1JQyE3wJiSgt1aCYMQdFPcGvDWtpXaZLYxAca",
+    "Patoshi Team": "EeZ3QLEkntPd3YBuhbQnaDQNiJgLp147pU6CjTiFti7i",
+    "Patoshi Team": "C2Sm2Xhwrwv395yZjYHp97tQ5bHpMvRbr7aL6RvoCayd",
+    "Patoshi Team": "BL73Ds3RPBenjeMxULKaafTGCoLXfn6G6WxrQhktaYkJ",
+    "Patoshi Team": "GVyBBoE1oNFkLdP7c9NcZTHqg9uSSDpSwYM9iED8Cpap",
+    "Patoshi Team": "62UWXo9spF2xi5g3mx536oQPPwoUm4XQTXv11ybAZUNq",
+    "Patoshi Team": "222dru1WfJfMpibTYME27fefYxd1gp3g11zatKh8ZKwa",
+    "Patoshi Team": "EoqopdFz41xo9SEhS8YZ2QzvcsCJXySMeZcgG9WzqG2s",
+    "Patoshi Team": "EoqopdFz41xo9SEhS8YZ2QzvcsCJXySMeZcgG9WzqG2s",
+    "Patoshi Team": "HmveAjHSFhuJWnV6pE14c2hKLkm2xnz3M371BBcTQqgG",
+    "Patoshi Team": "91nv5c7oXJuMGnPCrwiNPTJ7igxtdGeHoof7t2YLqtJK",
+    "Patoshi Team": "3BFBXFDZKA4vvg6XvgMqjAZJony1yPaFiB5J2hCqQd6E",
+    
+    
+    
+    
+    
 }
