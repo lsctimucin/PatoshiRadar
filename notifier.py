9be41d9af1594ddb651f7aa7a707ed4e9e1e6e33
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-08-03 11:31:05 +0300

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,14 +1,46 @@
-def build_message(name, symbol, market_cap, mint, creator, reason):
+def build_message(
+    name,
+    symbol,
+    market_cap,
+    mint,
+    creator,
+    creator_name,
+    keyword,
+):
+
+    reasons = []
+
+    if creator_name:
+        reasons.append(
+            f"🎯 Creator Match\n{creator_name}"
+        )
+
+    if keyword:
+        reasons.append(
+            f"🔍 Keyword Match\n{keyword}"
+        )
+
+    reason_text = "\n\n".join(reasons)
 
     return f"""🚀 Yeni Coin!
 
-🎯 Sebep : {reason}
+{reason_text}
 
-📛 İsim : {name}
-💎 Sembol : {symbol}
-💰 Market Cap : {market_cap:.2f} SOL
+━━━━━━━━━━━━━━
+
+📛 İsim
+{name}
+
+💎 Sembol
+{symbol}
+
+💰 Market Cap
+{market_cap:.2f} SOL
+
+━━━━━━━━━━━━━━
 
 👤 Creator
+
 {creator}
 
 https://pump.fun/{mint}
