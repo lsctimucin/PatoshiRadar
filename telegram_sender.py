148b8abe7658b4e8cdc8fc8ac7fef364b419d37d
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-08-03 11:20:22 +0300

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -10,21 +10,28 @@ class TelegramSender:
 
     def send(self, text):
 
-        response = requests.post(
-            f"https://api.telegram.org/bot{self.token}/sendMessage",
-            json={
-                "chat_id": self.chat_id,
-                "text": text
-            },
-            timeout=10
-        )
+        try:
+            response = requests.post(
+                f"https://api.telegram.org/bot{self.token}/sendMessage",
+                json={
+                    "chat_id": self.chat_id,
+                    "text": text
+                },
+                timeout=10
+            )
 
-        print("Telegram:", response.status_code)
-        print("Telegram Response:", response.text)
+            print("Telegram:", response.status_code)
+            print("Telegram Response:", response.text)
+
+            return response.status_code == 200
+
+        except requests.RequestException as e:
+            print("Telegram Hatası:", e)
+            return False
 
 
 sender = TelegramSender(BOT_TOKEN, CHAT_ID)
 
 
 def send_message(text):
-    sender.send(text)
+    return sender.send(text)
