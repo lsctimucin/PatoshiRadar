b591f8e6749470d09a8980e4ad51d0a4e2283be0
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-08-07 15:35:50 +0300

    Update creator_tracker.py

--- creator_tracker.py
+++ creator_tracker.py
@@ -8,6 +8,7 @@ def update_creator(creator):
     now = datetime.utcnow()
 
     if creator not in creator_history:
+
         creator_history[creator] = {
             "count": 1,
             "first_seen": now,
@@ -17,12 +18,13 @@ def update_creator(creator):
         return {
             "count": 1,
             "is_new": True,
-            "minutes": 0
+            "minutes": 0,
+            "seconds": 0
         }
 
-    last_seen = creator_history[creator]["last_seen"]
+    last = creator_history[creator]["last_seen"]
 
-    minutes = int((now - last_seen).total_seconds() / 60)
+    seconds = int((now - last).total_seconds())
 
     creator_history[creator]["count"] += 1
     creator_history[creator]["last_seen"] = now
@@ -30,5 +32,6 @@ def update_creator(creator):
     return {
         "count": creator_history[creator]["count"],
         "is_new": False,
-        "minutes": minutes
+        "minutes": seconds // 60,
+        "seconds": seconds
     }
