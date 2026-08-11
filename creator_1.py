3d65d4412f3516bd6cceafe50962b855bc56352b
lsctimucin <149061977+lsctimucin@users.noreply.github.com>
2026-08-11 12:30:02 +0300

    Update creator_1.py

--- creator_1.py
+++ creator_1.py
@@ -30,6 +30,7 @@ TARGET_CREATORS = {
     "Patoshi Team": "91nv5c7oXJuMGnPCrwiNPTJ7igxtdGeHoof7t2YLqtJK",
     "Patoshi Team": "3BFBXFDZKA4vvg6XvgMqjAZJony1yPaFiB5J2hCqQd6E",
     "Patoshi Team": "3etmwgxP4Lt2LLEyYpN2f9oKKi1onGy5XHRSP4BRq2vb",
+    "Patoshi Team": "67piDZqBKZgZE223KhCbo6pgXvqUnVF3LEECf1iwyU6C"
     
     
     
